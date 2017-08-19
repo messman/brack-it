b3c9cb51bd32bd83fd5b7e01b30e0940f74f7203
Andrew Messier <andrewgmessier@gmail.com>
2017-08-19 18:57:20 -0400

    Remove dead code

--- webpack.base.js
+++ webpack.base.js
@@ -61,15 +61,6 @@ const baseWebpackOptions = {
 			name: "runtime"
 		})
 	]
-
-	// When importing a module whose path matches one of the following, just
-	// assume a corresponding global variable exists and use that instead.
-	// This is important because it allows us to avoid bundling all of our
-	// dependencies, which allows browsers to cache those libraries between builds.
-	//externals: {
-	//	"react": "React",
-	//	"react-dom": "ReactDOM"
-	//},
 };
 
 module.exports = {
