f3e505e24e203b3d31096ec1f8405d0030882e38
Andrew Messier <andrewgmessier@gmail.com>
2017-08-19 15:21:26 -0400

    Add SCSS
    - Install node-sass, css/sass/style loaders
    - Install clean plugin to clean the dist directory
    - Install extract text for production to split CSS into a separate file
    - Move TS into other folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require("path");
 
+const env = process.env.NODE_ENV;
+console.log("Environment: " + env);
+
+// Add the ExtractTextPlugin to move CSS out into their own files
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin({
+	filename: "index.css",
+});
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
 module.exports = {
 	entry: {
-		index: "./src/entries/index.tsx",
+		index: "./src/ts/entries/index.tsx",
 	},
 	output: {
 		filename: "webpack.entry.[name].js",
@@ -26,9 +36,27 @@ module.exports = {
 				test: /\.tsx?$/,
 				exclude: ["./node_modules/"],
 				loaders: ["babel-loader", "ts-loader"]
+			},
+
+			{
+				test: /\.scss$/,
+				use: extractSass.extract({
+					use: [{
+						loader: "css-loader" // Translates CSS into JS
+					}, {
+						loader: "sass-loader" // Compiles SCSS
+					}],
+					// Use style-loader in development
+					fallback: "style-loader" // Create styles from JS strings
+				})
 			}
 		]
 	},
+
+	plugins: [
+		new CleanWebpackPlugin(["dist"]),
+		extractSass
+	]
 
 	// When importing a module whose path matches one of the following, just
 	// assume a corresponding global variable exists and use that instead.
