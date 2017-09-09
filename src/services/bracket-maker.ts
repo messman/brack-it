20192e3f0d5aa5453126341e800454293ebd2119
Andrew Messier <andrewgmessier@gmail.com>
2017-09-09 16:36:01 -0400

    Add logic to support arbitrary group matchup sizes
    No UI stuff yet, just the default "2" grouping property. Fall back to "2" in cases where a higher number wouldn't let the math work - like 10 players in a 3 group.

--- src/services/bracket-maker.ts
+++ src/services/bracket-maker.ts
@@ -7,9 +7,11 @@ export interface BracketSelectionSorter {
 
 /** Options used to determine how the bracket will be created. */
 export interface BracketCreationOptions {
-	/** A sort method. */
+	/** How many players will play in one matchup. (default 2) */
+	grouping: number,
+	/** A sort method. (default null) */
 	selectionSort: BracketSelectionSorter,
-	/** A method to modify player positions post-sorting. */
+	/** A method to modify player positions post-sorting. (default randomizes) */
 	selectionMod: (input: Player[]) => Player[],
 }
 
@@ -29,6 +31,7 @@ const randomizer = function randomizer(players: Player[]): Player[] {
 }
 
 export const defaultBracketCreationOptions: BracketCreationOptions = {
+	grouping: 2,
 	selectionSort: null,
 	selectionMod: randomizer,
 }
@@ -57,17 +60,18 @@ export class BracketCreator {
 		if (options.selectionMod)
 			players = options.selectionMod(players);
 
-		if (!players || players.length < 2)
-			throw new Error("insufficient player input");
+		const grouping = options.grouping;
+		if (!players || players.length < grouping)
+			throw new Error("insufficient player input for grouping " + grouping);
 
 		const length = players.length;
 
 		const overall: Matchup[][] = [];
 		let tree: Matchup[][] = [];
 		// Get the number of first round matches we should have for a perfect bracket setup with no extra games (n).
-		const n = BracketCreator.firstRoundMatches(length);
+		const n = BracketCreator.firstRoundMatches(length, grouping);
 
-		// Starting with n < x <= n*2 matchups...
+		// Starting with n < x <= n*[grouping] matchups...
 		// Trying to get to n matchups in as few extra games and rounds as possible.
 
 		// Map each player to an object that can hold their last matchup (none).
@@ -78,20 +82,27 @@ export class BracketCreator {
 			}
 		});
 
-		if (length == n * 2) {
+		if (length == n * grouping) {
 			// Easiest case. E.g., n=8 and 2 players face off, and we have 16 teams - so each of the 16 can play to 8, then 4, then 2, then 1.
-			tree = BracketCreator.createMatchupTree(inputs, true);
+			tree = BracketCreator.createMatchupTree(inputs, true, grouping);
 		}
 		else {
 			// In most cases, it won't be as pretty. To deal, create a play-in round to deal with the extra players.
-			const dif = length - n;
+			// Calculate how many matches we'll need to create here.
+			let diff = (length - n) / (grouping - 1);
+			let additionalGrouping = grouping;
+			// If this number is an integer, we can use it. Otherwise, we'll need to fill in with 2-games instead of (grouping) games.
+			if (Math.floor(diff) !== diff) {
+				diff = length - n;
+				additionalGrouping = 2;
+			}
 			let additional: Input[] = [];
 			// Take enough for the difference *and* the matchup opponents needed.
-			for (let i = 0; i < dif * 2; i++) {
+			for (let i = 0; i < diff * additionalGrouping; i++) {
 				additional.push(inputs.pop());
 			}
 			// Create just the play-in round.
-			const additionalTree = BracketCreator.createMatchups(additional);
+			const additionalTree = BracketCreator.createMatchups(additional, additionalGrouping);
 			overall.push(additionalTree);
 			// Now that those players have a matchup, use the matchup instead of their original "player" for reference.
 			let additionalInputs = additionalTree.map<Input>(function (matchup, index) {
@@ -100,8 +111,9 @@ export class BracketCreator {
 					player: -1
 				}
 			});
+
 			// Create the overall perfect bracket now.
-			tree = BracketCreator.createMatchupTree([...additionalInputs, ...inputs], true);
+			tree = BracketCreator.createMatchupTree([...additionalInputs, ...inputs], true, grouping);
 		}
 
 		for (let i = 0; i < tree.length; i++)
@@ -114,7 +126,7 @@ export class BracketCreator {
 
 	// Inputs.length should be a perfect nth of the grouping.
 	/** Creates a perfect bracket (optionally as a tree) from the given inputs. */
-	private static createMatchupTree(inputs: Input[], recursive: boolean, grouping: number = 2): Matchup[][] {
+	private static createMatchupTree(inputs: Input[], recursive: boolean, grouping: number): Matchup[][] {
 		if (inputs.length === 1)
 			return [];
 
@@ -136,7 +148,7 @@ export class BracketCreator {
 	}
 
 	/** Creates a single round of matchups from the players. Expects no extras. */
-	private static createMatchups(inputs: Input[], grouping: number = 2): Matchup[] {
+	private static createMatchups(inputs: Input[], grouping: number): Matchup[] {
 		if (inputs.length === 1)
 			return [];
 
@@ -163,21 +175,24 @@ export class BracketCreator {
 	}
 
 	/** Returns the power of [grouping] less than (and not equal to) the number. E.g., (12, 2) => 8. */
-	private static firstRoundMatches(input: number, grouping: number = 2): number {
-		if (grouping === 2) {
-			// Bitwise shifting
-			let num = input;
-			num--;
-			num |= num >> 1;
-			num |= num >> 2;
-			num |= num >> 4;
-			num |= num >> 8;
-			num |= num >> 16;
-			num++;
-			return num / 2;
-		}
-		else {
-			throw new Error("not implemented");
-		}
+	private static firstRoundMatches(input: number, grouping: number): number {
+
+		// Works by taking log(base [grouping]), floor it, then raise [grouping] to that power.
+		// We get can get the log with the base of an arbitrary number by dividing by the same log(e) of that arbitrary number.
+		return Math.pow(grouping, Math.floor(Math.log(input) / Math.log(grouping)));
+
+		// Old way for base 2 - bitwise shifting. Keep around just in case.
+		//if (grouping === 2) {
+		//	// Bitwise shifting
+		//	let num = input;
+		//	num--;
+		//	num |= num >> 1;
+		//	num |= num >> 2;
+		//	num |= num >> 4;
+		//	num |= num >> 8;
+		//	num |= num >> 16;
+		//	num++;
+		//	return num / 2;
+		//}
 	}
 }
